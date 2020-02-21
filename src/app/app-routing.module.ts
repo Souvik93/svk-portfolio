029a56fc102293aa2728faffc9214986d6db2812
Souvik93 <Souvik.das@live.in>
2020-02-21 17:10:02 +0530

    CV Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 
-const routes: Routes = [{
+const routes: Routes = [
+ {
+   path: 'home',
+   component: HomeComponent
+ },
+{
+  path: 'resume',
+  component: ResumeComponent
+},
+{
   path: '',
   redirectTo: '/home',
   pathMatch: 'prefix'
 },
- {
-   path: 'home',
-   component: HomeComponent
- }];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
